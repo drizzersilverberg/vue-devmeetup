bd417895b3836b58683a8743274a088edd2de957
Dendi Handian <handian.dendi@gmail.com>
2018-08-10 10:54:33 +0700

    adding single meetup card

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title>
+            <h1 class="red--text">My Meetup</h1>
+          </v-card-title>
+          <v-card-media
+            src="https://cdn-images-1.medium.com/max/1201/1*OGCE-B4onkKZyAzf1Ge0Ew.png"
+            height="400px"
+          ></v-card-media>
+          <v-card-text>
+            <div class="info--text mb-2">17th July - 2018 - where it takes place</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="red white--text mb-3 mr-3">Register</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
