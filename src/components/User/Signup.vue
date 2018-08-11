25be4b78e781be94bc2b1606c9e64478974ae984
Dendi Handian <handian.dendi@gmail.com>
2018-08-11 19:51:34 +0700

    sign up form dummy

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,12 +1,86 @@
 <template lang="html">
-  <div>
-    <p>The SignUp Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Password Confirmation"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="confirmPassword"
+                      :rules="[comparePasswords]"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Sign Up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-}
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    },
+    computed: {
+      comparePasswords () {
+        return this.password !== this.confirmPassword ? 'Password do not match' : ''
+      }
+    },
+    methods: {
+      onSignUp () {
+        console.log({
+          email: this.email,
+          password: this.password,
+          confirmPassword: this.confirmPassword
+        })
+      }
+    }
+  }
 </script>
 
 <style lang="css">
