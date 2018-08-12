43b53b04c0302c87a503cc158b7f8a6620d67997
Dendi Handian <handian.dendi@gmail.com>
2018-08-12 14:21:27 +0700

    add error and loading to signin view

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -1,5 +1,10 @@
 <template lang="html">
   <v-container>
+    <v-layout row v-if="error">
+      <v-flex xs12 sm6 offset-sm3>
+        <app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
+      </v-flex>
+    </v-layout>
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
         <v-card>
@@ -55,6 +60,12 @@ export default {
   computed: {
     user () {
       return this.$store.getters.user
+    },
+    error () {
+      return this.$store.getters.error
+    },
+    loading () {
+      return this.$store.getters.loading
     }
   },
   watch: {
@@ -70,6 +81,10 @@ export default {
         email: this.email,
         password: this.password
       })
+    },
+    onDismissed () {
+      console.log('Dismissed Alert!')
+      this.$store.dispatch('clearError')
     }
   }
 }
