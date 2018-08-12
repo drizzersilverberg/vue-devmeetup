61980df481fa341dd6b9cf3db2a060cf0f5a2ac7
Dendi Handian <handian.dendi@gmail.com>
2018-08-12 13:57:03 +0700

    add error alert and loading to signup view

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,10 @@
 <template lang="html">
   <v-container>
+    <v-layout row v-if="error">
+      <v-flex xs12 sm6 offset-sm3>
+        <app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
+      </v-flex>
+    </v-layout>
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
         <v-card>
@@ -45,7 +50,12 @@
                 </v-layout>
                 <v-layout row>
                   <v-flex xs12>
-                    <v-btn class="red white--text" type="submit">Sign Up</v-btn>
+                    <v-btn class="red white--text" type="submit" :disabled="loading" :loading="loading" >
+                      Sign Up
+                      <span slot="loader" class="custom-loader">
+                        <v-icon light>cached</v-icon>
+                      </span>
+                    </v-btn>
                   </v-flex>
                 </v-layout>
               </form>
@@ -72,6 +82,12 @@
       },
       user () {
         return this.$store.getters.user
+      },
+      error () {
+        return this.$store.getters.error
+      },
+      loading () {
+        return this.$store.getters.loading
       }
     },
     watch: {
@@ -87,6 +103,10 @@
           email: this.email,
           password: this.password
         })
+      },
+      onDismissed () {
+        console.log('Dismissed Alert!')
+        this.$store.dispatch('clearError')
       }
     }
   }
